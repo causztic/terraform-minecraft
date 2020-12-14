c54f979ed09dd22d1b5d2c2b498aedaa981a1bae
yaojie <limyaojie93@gmail.com>
2020-12-14 22:22:18 +0800

    stop myself from over-engineering

--- main/main.tf
+++ main/main.tf
@@ -12,13 +12,13 @@ data "terraform_remote_state" "volume" {
     dynamodb_table = "tf-locks"
   }
 }
-data "aws_ami" "ecs" {
+data "aws_ami" "ami" {
   most_recent = true
   owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn-ami*amazon-ecs-optimized"]
+    values = ["amzn-ami*"]
   }
 
   filter {
@@ -32,75 +32,18 @@ data "aws_ami" "ecs" {
   }
 }
 
-resource "aws_launch_configuration" "ecs_launch_config" {
-  image_id                    = data.aws_ami.ecs.id
-  ebs_optimized               = true
-  associate_public_ip_address = true
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ami.id
   instance_type               = "t3.nano"
-}
+  vpc_security_group_ids      = [aws_security_group.minecraft.id]
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public.id
 
-resource "aws_autoscaling_group" "asg" {
-  vpc_zone_identifier  = [aws_subnet.public.id]
-  launch_configuration = aws_launch_configuration.ecs_launch_config.name
-
-  desired_capacity = 1
-  min_size         = 1
-  max_size         = 1
-
-  tag {
-    key                 = "AmazonECSManaged"
-    value               = ""
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_ecs_capacity_provider" "minecraft" {
-  name = "minecraft"
-
-  auto_scaling_group_provider {
-    auto_scaling_group_arn = aws_autoscaling_group.asg.arn
-  }
-}
-
-resource "aws_ecs_cluster" "minecraft" {
-  name = "minecraft-cluster"
-  default_capacity_provider_strategy {
-    capacity_provider = "minecraft"
-  }
-}
-resource "aws_ecs_service" "minecraft" {
-  name            = "minecraft"
-  cluster         = aws_ecs_cluster.minecraft.id
-  task_definition = aws_ecs_task_definition.minecraft.arn
-  desired_count   = 1
-
-  placement_constraints {
-    type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [ap-southeast-1a, ap-southeast-1b, ap-southeast-1c]"
+  tags = {
+    Name = "minecraft"
   }
 
-  network_configuration {
-    subnets = [aws_subnet.public.id]
-    security_groups = [aws_security_group.minecraft.id]
-  }
-}
-
-resource "aws_ecs_task_definition" "minecraft" {
-  family                = "minecraft"
-  container_definitions = file("task-definitions/service.json")
-  requires_compatibilities = [ "EC2" ]
-  network_mode             = "awsvpc"
-
-  volume {
-    name = "minecraft-volume"
-
-    docker_volume_configuration {
-      scope = "shared"
-      autoprovision = "false"
-      driver = "rexray/ebs"
-      driver_opts = {
-        volumeID = data.terraform_remote_state.volume.outputs.ebs_volume_id
-      }
-    }
-  }
+  user_data = file("scripts/startup.sh")
+  depends_on = [aws_internet_gateway.gateway]
 }
