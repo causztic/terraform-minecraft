136daa1764816cc1759980e85ecfdd98786f351b
yaojie <limyaojie93@gmail.com>
2020-12-18 00:38:00 +0800

    source to minecraft directory

--- main/scripts/startup.sh
+++ main/scripts/startup.sh
@@ -6,12 +6,14 @@ mount ${device_name} /minecraft
 chown /minecraft
 chmod -R 700 /minecraft
 
-if [ ! -f "/minecraft/server.jar" ]; then
-  wget -P /minecraft https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
+source /minecraft
+
+if [ ! -f "server.jar" ]; then
+  wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
   # handle eula text
-  java -Xms512M -Xmx512M -jar /minecraft/server.jar nogui
-  echo eula=true > /minecraft/eula.txt
+  java -Xms512M -Xmx512M -jar server.jar nogui
+  echo eula=true > eula.txt
   # TODO: update server.properties
 fi
 
-setsid nohup java -Xms${min} -Xmx${max} -jar /minecraft/server.jar nogui &
+setsid nohup java -Xms${min} -Xmx${max} -jar server.jar nogui &
